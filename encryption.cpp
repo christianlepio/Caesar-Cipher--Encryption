2e535f41b2aae3c7673f981464fa73b1a28205a3
babaGie <rylepio79@gmail.com>
2022-05-06 06:48:01 +0800

    update encryption source code

--- encryption.cpp
+++ encryption.cpp
@@ -5,16 +5,56 @@ int main()
 {
 	//Encryption
 	string text;
+	int numkey=0;
 	
 	cout << "Enter a plaintext: ";
 	getline(cin , text);
+	cout<<"\n Enter your desire number key: ";
+	cin>>numkey;
 	
 	for(int count = 0; count < text.length() ; count++)
 	{
 		text[count] = tolower(text[count]);
 		if(isalpha(text[count]))
 		{
-			for(int i = 1 ; i <= 3 ; i++)
+			for(int i = 1 ; i <= numkey ; i++)
+			{
+				if(text[count] == 'z')
+					text[count] = 'a';
+				else
+					++text[count];
+			}
+		}
+		
+	}
+	cout << "\n\aEquivalent ciphertext: " << text << endl;
+	
+	
+	
+	
+	
+	return 0;
+}
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//Encryption
+	string text;
+	int numkey=0;
+	
+	cout << "Enter a plaintext: ";
+	getline(cin , text);
+	cout<<"\n Enter your desire number key: ";
+	cin>>numkey;
+	
+	for(int count = 0; count < text.length() ; count++)
+	{
+		text[count] = tolower(text[count]);
+		if(isalpha(text[count]))
+		{
+			for(int i = 1 ; i <= numkey ; i++)
 			{
 				if(text[count] == 'z')
 					text[count] = 'a';
